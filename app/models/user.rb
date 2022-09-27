7e87c6b6864ef082cc16d647600f0a6a9ec82960
misokatsusand <dam_do_4646@yahoo.co.jp>
2022-09-28 03:29:51 +0900

    refactor app/models/user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,16 +10,14 @@ class User < ApplicationRecord
   validates :power, numericality: { greater_than_or_equal_to: 1000, less_than_or_equal_to: 4000 }, allow_nil: true
 
   def self.create_or_update_from_auth(auth)
-    begin
-      find_or_initialize_by(uid: auth[:uid]).tap do |user|
-        user.update!(
-          nickname: auth[:info][:nickname],
-          name: auth[:info][:name],
-          image: auth[:info][:image]
-        )
-      end
-    rescue
-      return nil
+    find_or_initialize_by(uid: auth[:uid]).tap do |user|
+      user.update!(
+        nickname: auth[:info][:nickname],
+        name: auth[:info][:name],
+        image: auth[:info][:image]
+      )
     end
+  rescue StandardError
+    nil
   end
 end
